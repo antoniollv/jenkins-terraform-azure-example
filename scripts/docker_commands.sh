d594edaecc4f60728c72f57dd54ad34d51537adb
Antonio Lledó Valero <antonio.lledo.valero@accenture.com>
2023-02-15 17:10:50 +0100

    start script docker_commands.sh

---
+++ scripts/docker_commands.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#Referencias:
+#https://www.digitalocean.com/community/tutorials/how-to-automate-jenkins-setup-with-docker-and-jenkins-configuration-as-code
+
+#Establecer las variables de entorno en la sesión de construcción de la imagen. en el ejemplo actual ejecutar
+. ./set_evironment_variables.sh
+
+# build image
+docker build \
+-t jenkinsalfa:latest \
+--build-arg ARG_ARM_CLIENT_ID=$ARM_CLIENT_ID \
+--build-arg ARG_ARM_CLIENT_SECRET=$ARM_CLIENT_SECRET \
+--build-arg ARG_ARM_SUBSCRIPTION_ID=$ARM_SUBSCRIPTION_ID \
+--build-arg ARG_ARM_TENANT_ID=$ARM_TENANT_ID \
+--build-arg ARG_JENKINS_ADMIN_ID=$JENKINS_ADMIN_ID \
+--build-arg ARG_JENKINS_ADMIN_PASSWORD=$JENKINS_ADMIN_PASSWORD \
+--no-cache .
+
+#docker create --name jenkins_alfa -p 8080:8080 jenkinsalfa:latest
+#docker start jenkins_alfa
+
+#Commands AZ CLI
+
+resourceGroup="gr_jenkins_alfa"
+aciStorageAccountName="sa_jenkins_alfa"
+location="westeurope"
+acrName="acr_jenkins_alfa"
+
+az account set -s $ARM_SUBSCRIPTION_ID
+
+az group create --l $location \
+    -n $resourceGroup \
+    --tags "Project=Jenkins_alfa"
+
+az storage account create \
+    -g $resourceGroup \
+    -n $aciStorageAccountName \
+    --location $location \
+    --sku Standard_LRS
+
+# Create the file share
+#az storage share create \
+#  --name $ACI_FILE_SHARE_NAME \
+#  --account-name $aciStorageAccountName \
+#  -o none 
+
+# create acr
+az acr create -n $acrName \
+    -g $resourceGroup \
+    --sku Standard \
+    --admin-enabled
+
+# get login server
+acrLoginServer=$(az acr show --name $acrName --query loginServer -o tsv)
+
+# re-tag jenkins image
+docker tag jenkinsalfa:lts "$acrLoginServer/jenkinsalfa:lts"
+
+# get acr admin user
+acrAdminUser=$(az acr credential show -n $acrName -g $resourceGroup --query username -o tsv)
+
+# get acr admin password
+acrAdminPassword=$(az acr credential show -n $acrName -g $resourceGroup --query passwords[0].value -o tsv)
+
+# log into acr
+az acr login --name $acrName
+
+# push image to acr
+echo "pushing the jenkins image to azure container registry"
+docker push "$acrLoginServer/jenkinsalfa:lts"
+
+# get aci storage account key
+echo "getting aci storage account key"
+aciStorageAccountKey=$(az storage account keys list --resource-group $resourceGroup --account-name $aciStorageAccountName --query "[0].value" --output tsv)
+
+# deploy to aci
+echo "deploying the jenkins image to azure container instances"
+az container create --resource-group $resourceGroup \
+    --name jenkins_alfa \
+    --image "$acrLoginServer/jenkinsalfa:lts" \
+    --cpu 1 --memory 5 \
+    --registry-login-server $acrLoginServer \
+    --registry-username $acrAdminUser \
+    --registry-password $acrAdminPassword \
+    --dns-name-label "jenkinsalfa-dns" \
+    --ports 8080 8080 \
+    --azure-file-volume-account-name $aciStorageAccountName \
+    --azure-file-volume-account-key $aciStorageAccountKey 
+
+    #--azure-file-volume-share-name $ACI_FILE_SHARE_NAME \
+    #--azure-file-volume-mount-path /var/jenkins_home/
+
+# get aci fdqn
+aciFdqn=$(az container show --resource-group $resourceGroup --name $aciName --query ipAddress.fqdn -o tsv)
+
+
